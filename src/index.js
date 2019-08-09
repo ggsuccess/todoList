e64a21bab7d031f7f90bd77c6915afaf5033dd05
ggsuccess <ggsuccess@naver.com>
2019-08-09 13:48:15 +0900

    delete unnecessary.......

--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+class TodoListItem extends React.Component{
+  constructor(props){
+    super(props)
+      this.state = {
+        done : false
+      }
+    }
+  
+  onListItemClick(){
+    this.setState({
+      done : !this.state.done
+    });
+  }
+  render(){
+    const style = {
+      //textDecoration : this.state.done ? 'line-through':'none'
+      backgroundColor : this.state.done ? 'red':'blue'
+    }
+    return(
+      <div>
+        <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.todos}</li>
+      </div>
+    )
+  }
+}
+
+// const TodoList = (props) => (
+//   <ul>
+//     {props.todos.map(todo => <TodoListItem todo={todo}/>
+//     )}
+//   </ul>
+// )
+const App = () => (
+  <div>
+    <h2>TodoList</h2>
+      <TodoListItem
+      todos={[
+        ['cucumber'],['kale'],['diamond']
+      ]}
+      />
+  </div>
+)
+// const TodoList = (props) => {
+//   const onListItemClick = (event) =>{
+//     console.log('I got clicked!');
+//   };
+//   return(
+//     <ul>
+//       <li onClick={onListItemClick}>{props.todos[0]}</li> 
+//       <li>{props.todos[1]}</li>
+//       <li>{props.todos[2]}</li>
+//     </ul>
+//   )
+// };
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
